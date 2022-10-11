6ab308dfa8c2e39d011b167b2cc704fe49651c74
fsabirov <sabirovfarit@gmail.com>
2022-10-11 17:58:01 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const readline = require('readline');
+
+const myAPIKey = process.env.myAPIKey;
+
+const rl = readline.createInterface(process.stdin, process.stdout);
+
+rl.question(`Задайте город, чтобы посмотреть погоду \n`, (userInput) => {
+
+const url = `http://api.weatherstack.com/current?access_key=${myAPIKey}&query=${userInput}`;
+
+  http.get(url, res => {
+
+    res.setEncoding('utf8');
+
+    let rowData = '';
+    let parseData = '';
+    res.on('data', (chunk) => rowData += chunk)
+    res.on('end', () => {
+      parseData = JSON.parse(rowData)
+      if (!parseData.error) {
+        console.log(`Запрашиваемые данные по городу: ${userInput}`)
+        console.log(parseData);
+        rl.close();
+      }else if(+parseData.error.code===615){
+        console.log('Неверное название города!');
+        rl.close();
+      }else {
+        console.log('Технические проблемы!');
+        rl.close();
+      }
+    })
+  }).on('error', err => {
+    console.error(err);
+  })
+});
+
